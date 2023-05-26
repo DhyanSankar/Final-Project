6d66672723f39e769a17eb1a8c3bc28dd5363b85
Dhyan Sankar <dhyansankar007@gmail.com>
2023-05-26 17:18:13 -0400

    Made databases
    Co-authored-by: Faizan-Sayed <Faizan-Sayed@users.noreply.github.com>

--- db/db_create.js
+++ db/db_create.js
@@ -2,6 +2,18 @@ const db = require("./db_connection");
 
 /**** Drop existing tables, if any ****/
 
+const drop_cart_table_sql = "DROP TABLE IF EXISTS cart;"
+
+db.execute(drop_cart_table_sql);
+
+const drop_food_table_sql = "DROP TABLE IF EXISTS food;"
+
+db.execute(drop_food_table_sql);
+
+const drop_reviews_table_sql = "DROP TABLE IF EXISTS reviews;"
+
+db.execute(drop_reviews_table_sql);
+
 const drop_restaurants_table_sql = "DROP TABLE IF EXISTS restaurants;"
 
 db.execute(drop_restaurants_table_sql);
@@ -11,11 +23,60 @@ db.execute(drop_restaurants_table_sql);
 const create_restaurants_table_sql = `
     CREATE TABLE restaurants (
         restaurant_id INT NOT NULL AUTO_INCREMENT,
-        restaurant_name VARCHAR(45) NOT NULL,
+        restaurant_name VARCHAR(63) NOT NULL,
         restaurant_food_type VARCHAR(255) NULL,
         restaurant_location VARCHAR(255) NULL,
         PRIMARY KEY (restaurant_id));
 `
 db.execute(create_restaurants_table_sql);
+
+const create_food_table_sql = `
+    CREATE TABLE food (
+        food_id INT NOT NULL AUTO_INCREMENT,
+        restaurant_id INT NOT NULL,
+        food_name VARCHAR(63) NOT NULL,
+        food_description VARCHAR(255) NULL,
+        food_allergens VARCHAR(63) NULL,
+        PRIMARY KEY (food_id),
+        INDEX foodRestaurant_idx (restaurant_id ASC),
+        CONSTRAINT foodRestaurant
+            FOREIGN KEY (restaurant_id)
+            REFERENCES restaurants (restaurant_id)
+            ON DELETE RESTRICT
+            ON UPDATE CASCADE);
+`
+db.execute(create_food_table_sql);
+
+const create_reviews_table_sql = `
+    CREATE TABLE reviews (
+        review_id INT NOT NULL AUTO_INCREMENT,
+        restaurant_id INT NOT NULL,
+        user_id INT NOT NULL,
+        review_score INT NOT NULL,
+        review_description VARCHAR(255) NULL,
+        PRIMARY KEY (review_id),
+        INDEX reviewRestaurant_idx (restaurant_id ASC),
+        CONSTRAINT reviewRestaurant
+            FOREIGN KEY (restaurant_id)
+            REFERENCES restaurants (restaurant_id)
+            ON DELETE RESTRICT
+            ON UPDATE CASCADE);
+`
+db.execute(create_reviews_table_sql);
+
+const create_cart_table_sql = `
+    CREATE TABLE cart (
+        purchase_id INT NOT NULL AUTO_INCREMENT,
+        user_id INT NOT NULL,
+        food_id INT NOT NULL,
+        PRIMARY KEY (purchase_id),
+        INDEX cartFood_idx (food_id ASC),
+        CONSTRAINT cartFood
+            FOREIGN KEY (food_id)
+            REFERENCES food (food_id)
+            ON DELETE RESTRICT
+            ON UPDATE CASCADE);
+`
+db.execute(create_cart_table_sql);
 
 db.end();
--- db/db_insert_sample_data.js
+++ db/db_insert_sample_data.js
@@ -6,6 +6,56 @@ const delete_restaurants_table_sql = "DELETE FROM restaurants;"
 
 db.execute(delete_restaurants_table_sql);
 
-const delete_subjects_table_sql = "DELETE FROM subjects;"
+const delete_food_table_sql = "DELETE FROM food;"
 
-db.execute(delete_subjects_table_sql);
+db.execute(delete_food_table_sql);
+
+const insert_restaurants_sql = `
+    INSERT INTO restaurants
+    (restaurant_id,  restaurant_name, restaurant_food_type, restaurant_location)
+    VALUES
+    (?, ?, ?, ?);
+`
+
+db.execute(insert_restaurants_sql, [1, 'Restaurant #1', 'Italian', '206 Crest Drive, Paramus, NJ']);
+
+db.execute(insert_restaurants_sql, [2, 'Restaurant #2', 'Breads', '14 Truman Court, Closter, NJ']);
+
+db.execute(insert_restaurants_sql, [3, 'Restaurant #3', 'Ice Cream', '555 Marion Street, Teaneck, NJ']);
+
+const insert_food_sql = `
+    INSERT INTO food
+    (food_id, restaurant_id, food_name, food_description, food_allergens, )
+    VALUES
+    (?, ?, ?, ?, ?);
+    `
+
+db.execute(insert_food_sql, [1, 1, 'pizza', 'what\'s better than a pie? a pie WITH CHEESE']);
+
+db.execute(insert_food_sql, [2, 1, 'pasta', 'tastes a little like spaghetti']);
+
+db.execute(insert_food_sql, [3, 1, 'spaghetti', 'tastes a little like pasta']);
+
+db.execute(insert_food_sql, [4, 2, 'white bread', 'crafted and designed to feed geese']);
+
+db.execute(insert_food_sql, [5, 2, 'wheat bread', 'for people who want to think they\'re heathier than the rest']);
+
+db.execute(insert_food_sql, [6, 2, 'baguette', 'loooong bread']);
+
+db.execute(insert_food_sql, [7, 3, 'strawberry ice cream', 'ice cream but it tastes liek strawberries']);
+
+db.execute(insert_food_sql, [8, 3, 'CHOCOLATE ice cream', 'ice cream but it tastes liek CHOCOLATE']);
+    
+db.execute(insert_food_sql, [9, 3, 'vanilla ice cream sandwich', 'ice cream smodged between 2 pieces of chocolate thingies']);
+
+const insert_reviews_sql = `
+    INSERT INTO reviews
+    (review_id, restaurant_id, user_id, review_score, review_description)
+    VALUES
+    (?, ?, ?, ?, ?);
+    `
+
+
+    db.execute(insert_reviews_sql, [1, 1, 1, 10, 'food is GOOD']);
+    
+    db.execute(insert_reviews_sql, [1, 1, 1, 10, 'food is GOOD']);
